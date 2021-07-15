e3ce6490becb56f2050e6d97d53decaa82845634
Pythonology <pythonology@gmail.com>
2021-07-15 18:40:57 -0400

    Add BinaryRecords extensions, and rename RabbitMQ extensions -> Autobus.RabbitMQ

---
+++ BeatTogether.Extensions.BinaryRecords/BinaryBufferReaderExtensions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using BinaryRecords;
+
+namespace BeatTogether.Extensions
+{
+    public static class BinaryBufferReaderExtensions
+    {
+        public static ulong ReadVarULong(this ref BinaryBufferReader bufferReader)
+        {
+            var value = 0UL;
+            var shift = 0;
+            var b = bufferReader.ReadByte();
+            while ((b & 128UL) != 0UL)
+            {
+                value |= (b & 127UL) << shift;
+                shift += 7;
+                b = bufferReader.ReadByte();
+            }
+            return value | (ulong)b << shift;
+        }
+
+        public static long ReadVarLong(this ref BinaryBufferReader bufferReader)
+        {
+            var varULong = (long)bufferReader.ReadVarULong();
+            if ((varULong & 1L) != 1L)
+                return varULong >> 1;
+            return -(varULong >> 1) + 1L;
+        }
+
+        public static uint ReadVarUInt(this ref BinaryBufferReader bufferReader)
+            => (uint)bufferReader.ReadVarULong();
+
+        public static int ReadVarInt(this ref BinaryBufferReader bufferReader)
+            => (int)bufferReader.ReadVarLong();
+
+        public static bool TryReadVarULong(this ref BinaryBufferReader bufferReader, out ulong value)
+        {
+            value = 0UL;
+            var shift = 0;
+            while (shift <= 63 && bufferReader.RemainingSize >= 1)
+            {
+                var b = bufferReader.ReadByte();
+                value |= (ulong)(b & 127) << shift;
+                shift += 7;
+                if ((b & 128) == 0)
+                    return true;
+            }
+
+            value = 0UL;
+            return false;
+        }
+
+        public static bool TryReadVarUInt(this ref BinaryBufferReader bufferReader, out uint value)
+        {
+            ulong num;
+            if (bufferReader.TryReadVarULong(out num) && (num >> 32) == 0UL)
+            {
+                value = (uint)num;
+                return true;
+            }
+
+            value = 0U;
+            return false;
+        }
+
+        public static ReadOnlySpan<byte> ReadVarBytes(this ref BinaryBufferReader bufferReader)
+        {
+            var length = bufferReader.ReadVarUInt();
+            return bufferReader.ReadBytes((int)length);
+        }
+
+        public static string ReadString(this ref BinaryBufferReader bufferReader, int maxLength = 65535)
+        {
+            var length = bufferReader.ReadInt32();
+            if (length <= 0 | length > maxLength)
+                return string.Empty;
+            var bytes = bufferReader.ReadBytes(length);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        public static IPEndPoint ReadIPEndPoint(this ref BinaryBufferReader bufferReader)
+        {
+            if (!IPAddress.TryParse(bufferReader.ReadString(512), out var address))
+                throw new ArgumentException("Failed to parse IP address");
+            var port = bufferReader.ReadInt32();
+            return new IPEndPoint(address, port);
+        }
+    }
+}
---
+++ BeatTogether.Extensions.BinaryRecords/BinaryBufferWriterExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using BinaryRecords;
+
+namespace BeatTogether.Extensions
+{
+    public static class BinaryBufferWriterExtensions
+    {
+        public static void WriteVarULong(this ref BinaryBufferWriter bufferWriter, ulong value)
+        {
+            do
+            {
+                var b = (byte)(value & 127UL);
+                value >>= 7;
+                if (value != 0UL)
+                    b |= 128;
+                bufferWriter.WriteUInt8(b);
+            } while (value != 0UL);
+        }
+
+        public static void WriteVarLong(this ref BinaryBufferWriter bufferWriter, long value)
+            => bufferWriter.WriteVarULong((value < 0L ? (ulong)((-value << 1) - 1L) : (ulong)(value << 1)));
+
+        public static void WriteVarUInt(this ref BinaryBufferWriter buffer, uint value)
+            => buffer.WriteVarULong(value);
+
+        public static void WriteVarInt(this ref BinaryBufferWriter bufferWriter, int value)
+            => bufferWriter.WriteVarLong(value);
+
+        public static void WriteVarBytes(this ref BinaryBufferWriter bufferWriter, ReadOnlySpan<byte> bytes)
+        {
+            bufferWriter.WriteVarUInt((uint)bytes.Length);
+            bufferWriter.WriteBytes(bytes);
+        }
+
+        public static void WriteString(this ref BinaryBufferWriter bufferWriter, string value)
+        {
+            bufferWriter.WriteInt32(Encoding.UTF8.GetByteCount(value));
+            bufferWriter.WriteBytes(Encoding.UTF8.GetBytes(value));
+        }
+
+        public static void WriteIPEndPoint(this ref BinaryBufferWriter bufferWriter, IPEndPoint ipEndPoint)
+        {
+            bufferWriter.WriteString(ipEndPoint.Address.ToString());
+            bufferWriter.WriteInt32(ipEndPoint.Port);
+        }
+    }
+}
